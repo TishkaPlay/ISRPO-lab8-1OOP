e6ab830fbbd87a9b0629c1d32c9db540b1e3bff8
TishkaaPlay <tikhon42otmyanin@gmail.com>
2025-12-02 18:19:12 +0300

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP8
+{
+    class Person
+    {
+        string name = "Tom";
+        int age = 1;
+
+        public int Age { set { age = value; } }
+
+        public string Name { get { return name; } }
+
+        public void Print() => Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*namespace OOP8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.WriteLine(person.Name);
+            person.Age = 37;
+            person.Print();
+
+            var apple = new Product { Name = "Яблоко", Price = 10.5m, Quantity = 5 };
+            var banana = new Product { Name = "Банан", Price = 7.2m, Quantity = -2 };
+
+            Console.WriteLine($"{apple.Name} x {apple.Quantity} = {apple.TotalPrice} Руб.");
+            Console.WriteLine($"{banana.Name} x {banana.Quantity} = {banana.TotalPrice} Руб.");
+
+            Hero hero = new Hero("Ривен", 120);
+            Console.WriteLine($"Имя героя: {hero.Name}\nУровень героя: {hero.Level}\nМана: {hero.Mana}\nСтатус: {hero.Status}");
+            hero.TakeDamage(50);
+            hero.Heal(30);
+            hero.TakeDamage(200);
+            hero.Heal(50);
+        }
+    }
+}
+*/
